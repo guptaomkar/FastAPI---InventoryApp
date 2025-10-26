40405cf811ee1cc5c7d41872c1a01a7b03007697
Omkar Gupta <Omkar.gupta1999@gmail.com>
2025-10-27 02:59:24 +0530

    Update CORS settings to allow all origins

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Allow all headers (like Content-Type, Authorization)
